51a831b7a0418cad9f39910465bbbd03fe0634a2
Adam Grabski <adam.gr@outlook.com>
2021-12-22 19:30:22 +0100

    working demo

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-from 'agents/'
+from agents.dummy import DummyAgent
+from spade import quit_spade
+
+dummy = DummyAgent("your_jid@your_xmpp_server", "your_password")
+future = dummy.start()
+future.result()
+
+dummy.stop()
+quit_spade()

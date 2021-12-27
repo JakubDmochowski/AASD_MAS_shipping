7fd0736395e9474b542d4e8a767d5d0e03b9f52a
Adam Grabski <adam.gr@outlook.com>
2021-12-27 15:17:58 +0100

    Działający przyklad

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Instalacja
+
+W konfiguracji Tigrase XMPP należy dodać domenę localhost (przynajmniej na windows inaczej nie działa).
+Nie trzeba dodawać żadnych użytkowników, agent sam się zarejestruje.
--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from agents.dummy import DummyAgent
 from spade import quit_spade
 
-dummy = DummyAgent("your_jid@your_xmpp_server", "your_password")
+dummy = DummyAgent("ss@localhost", "aa")
 future = dummy.start()
 future.result()
 
 dummy.stop()
 quit_spade()
+

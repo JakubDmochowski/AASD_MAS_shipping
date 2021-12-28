289954d52b3d8df95e33ead118e490de99783d7c
Adam Grabski <adam.gr@outlook.com>
2021-12-28 18:18:28 +0100

    start

--- src/agents/dummy.py
+++ src/agents/dummy.py
@@ -1,5 +1,5 @@
 from spade import agent
 
 class DummyAgent(agent.Agent):
-    async def setup(self):
-        print("Hello World! I'm agent {}".format(str(self.jid)))
+	async def setup(self):
+		print("Hello World! I'm agent {}".format(str(self.jid)))
---
+++ src/messages/warehouseStateReport.py
@@ -0,0 +1,8 @@
+
+
+from typing import Dict
+
+
+class WarehouseStateReport:
+	def __init__(self, contents: Dict) -> None:
+		pass

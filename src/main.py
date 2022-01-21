d74d81e57dbb5c4601221c1a437eee30fac86613
Jakub Dmochowski <jakub@dmochowski.co>
2022-01-21 16:22:56 +0100

    add case4

--- src/main.py
+++ src/main.py
@@ -2,8 +2,10 @@ from spade import quit_spade
 from case_studies.case1.main import case1
 from case_studies.case2.main import case2
 from case_studies.case3.main import case3
+from case_studies.case4.main import case4
 
 case1()
 case2()
 case3()
+case4()
 quit_spade()

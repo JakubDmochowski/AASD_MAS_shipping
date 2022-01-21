43dde532b4ef9af1c64ae5d9336476b9c8b4527c
Kasia4 <katarzyna.rybacka4@gmail.com>
2022-01-21 15:21:17 +0100

    Add case 3

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from spade import quit_spade
 from case_studies.case1.main import case1
 from case_studies.case2.main import case2
+from case_studies.case3.main import case3
 
 case1()
 case2()
+case3()
 quit_spade()
